4472920c480f0c473a6e1643df6f08aae9b68759
saaddalida <dalidassaad@gmail.com>
2019-10-06 21:01:43 -0300

    List page

---
+++ src/components/SpotList.js
@@ -0,0 +1,96 @@
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  StyleSheet,
+  Text,
+  FlatList,
+  Image,
+  TouchableOpacity
+} from 'react-native';
+
+import api from '../services/api';
+
+export default function SpotList({ tech }) {
+  const [spots, setSpots] = useState([]);
+
+  useEffect(() => {
+    async function loadSpots() {
+      const response = await api.get('/spots', {
+        params: { tech }
+      });
+
+      setSpots(response.data);
+    }
+
+    loadSpots();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        Empresas que usam <Text style={styles.bold}>{tech}</Text>
+        <FlatList
+          style={styles.list}
+          data={spots}
+          keyExtractor={spot => spot._id}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          renderItem={({ item }) => (
+            <View style={styles.listItem}>
+              <Image
+                style={styles.thumbnail}
+                source={{ uri: item.thumbnail_url }}
+              />
+              <Text style={styles.company}>{item.company}</Text>
+              <Text style={styles.price}>
+                {item.price ? `R$${item.price}/dia` : 'GRATUITO'}
+              </Text>
+              <TouchableOpacity onPress={() => {}} style={styles.button}>
+                <Text style={styles.text}>Solicitar reserva</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        />
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 30
+  },
+  title: {
+    fontSize: 20,
+    color: '#444',
+    paddingHorizontal: 20,
+    marginBottom: 15
+  },
+  bold: {
+    fontWeight: 'bold'
+  },
+
+  list: {
+    paddingHorizontal: 20
+  },
+  listItem: {
+    marginRight: 15
+  },
+  thumbnail: {
+    width: 200,
+    height: 120,
+    resizeMode: 'cover',
+    borderRadius: 2
+  },
+  company: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+    marginTop: 10
+  },
+  price: {
+    fontSize: 15,
+    color: '#999',
+    marginTop: 5
+  }
+});
